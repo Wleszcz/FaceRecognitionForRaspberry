bed341f02dc742a1bc0d3a1314a8e9465ad176dd
Wleszcz <wiktor.leszczynski@tlen.pl>
2023-12-02 12:56:19 +0100

    LAB 4

--- aswapp.py
+++ aswapp.py
@@ -1,19 +1,18 @@
 from guizero import App, Picture, PushButton, Text
 import cv2
 from PIL import Image, ImageDraw
-import io
+from pyzbar.pyzbar import decode
+
+alpha = 1.3  # Contrast control (1.0-3.0)
+beta = 0  # Brightness control (0-100)
 
 
 class ASWApp:
     app: App
-    button: PushButton
-    count: Text
+    code: Text
     loaded_image: Image = None
-
-    def getfile(self):
-        path = self.app.select_file()
-        if path:
-            self.loaded_image = Image.open(path)
+    bd = cv2.barcode.BarcodeDetector()
+    last_code: str = ""
 
     def __init__(self):
         self.cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
@@ -22,15 +21,8 @@ class ASWApp:
         self.init_gui()
 
     def init_gui(self):
-        self.button = PushButton(self.app, command=self.getfile, text="Load Image")
-        self.count = Text(self.app, text="Shalom", size=20)
+        self.code = Text(self.app, text="Shalom", size=20)
         self.picture = Picture(self.app, image=None)
-
-    def find_faces(self, frame):
-        resized = cv2.resize(frame, (640, 480))
-        grayFrame = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
-        faces = self.facecascade.detectMultiScale(grayFrame, 1.1, 4)
-        return faces
 
     def update_gui(self):
         ret, frame = self.cap.read()
@@ -38,25 +30,33 @@ class ASWApp:
             return
 
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = cv2.convertScaleAbs(frame, alpha=alpha, beta=beta)
+
+        # frame = cv2.imread("assets/Wiki-ean13.png")
+        frame, s = self.getBarcode(frame)
+
+        if s and s != self.last_code:
+            print(s)
+            self.last_code = s
+            self.code.value = self.last_code
+            #TODO PRINT TO LCD
 
         img = Image.fromarray(frame)
-        img = self.draw_faces(img, self.find_faces(frame))
 
         img = img.resize((640, 480))
         self.picture.image = img
 
     def run(self):
-        self.app.repeat(100, self.update_gui)
+        self.app.repeat(110, self.update_gui)
         self.app.display()
 
-    def draw_faces(self, img, faces):
-        self.count.value = "Faces detected: " + str(len(faces))
-        for x, y, w, h in faces:
-            if self.loaded_image:
-                copy = self.loaded_image.copy()
-                copy = copy.resize((w, h))
-                img.paste(copy, (x, y), copy)
+    def getBarcode(self, frame):
+        s = ""
+        for d in decode(frame):
+            s = d.data.decode()
+            frame = cv2.rectangle(frame, (d.rect.left, d.rect.top),
+                                  (d.rect.left + d.rect.width, d.rect.top + d.rect.height), (0, 255, 0), 3)
+            frame = cv2.putText(frame, s, (d.rect.left, d.rect.top + d.rect.height),
+                                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
-            img1 = ImageDraw.Draw(img)
-            img1.rectangle(((x, y), (x + w, y + h)), outline="red")
-        return img
+        return frame, s
